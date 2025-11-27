4ab9b1645c57a2b1ecaa2314c268d3594aa31650
apriliann <achmadaji304@gmail.com>
2025-11-27 13:24:51 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,69 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+if(navToggle){
+      navToggle.addEventListener('click', () =>{
+      navMenu.classList.add('show-menu')
+})
+}
+if(navClose){
+      navClose.addEventListener('click', () =>{
+      navMenu.classList.remove('show-menu')
+})
+}
+
+const navLink = document.querySelectorAll('.nav__link')
+const linkAction = () =>{
+const navMenu = document.getElementById('nav-menu')
+// When we click on each nav_link, we remove the show-menu class
+navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+/*=============== ADD BLUR TO HEADER ===============*/
+const blurHeader = () =>{
+const header = document.getElementById('header')    
+this.scrollY >= 50 ? header.classList.add('blur-header')
+                        :header.classList.remove('blur-header')
+}
+window.addEventListener('scroll', blurHeader)
+
+/*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form'),
+      contactMassage = document.getElementById('contact-massage')
+
+const sendEmail = (e) =>{
+      e.preventDefault()
+
+}
+contactForm.addEventListener('submit',sendEmail)
+/*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp = () =>{
+      const scrollUp= document.getElementById('scroll-up')
+      // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup this.scrolly >= 350 ? scrollup.classList.add('show-scroll')
+      this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                                                : scrollUp.classList.remove('show-scroll')
+}
+      window.addEventListener('scroll', scrollUp)
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ERROR ===============*/
+const section = document.querySelectorAll('section[id]')
+
+const scrollActive =() => {
+      const scrollY = window.pageYOffset
+
+      section.forEach(current =>{
+            const sectionHeight = current.offsetHeight,
+                  sectionTop = current.offsetTop - 58,
+                  sectionId = current.getAttribute('id'),
+                  sectionClass = document.querySelector('nav__menu a [href*=' + sectionId + ']')
+
+                  if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+                        sectionClass.classList.add('active-link')
+                  }else{
+                        sectionClass.classList.remove('active-link')
+                  }
+      })
+}
+window.addEventListener('scroll', scrollActive)
+/*=============== SCROLL REVEAL ANIMATION ===============*/
